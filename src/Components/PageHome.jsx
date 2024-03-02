385f283821e3d3afaa3c8a596a71f16b23406cf1
potsamnang <potsamnang68@gmail.com>
2024-03-02 11:52:41 +0700

    commit first

--- src/Components/PageHome.jsx
+++ src/Components/PageHome.jsx
@@ -3,19 +3,22 @@ import HomeProp from "./HomeProp";
 import p1 from "../assets/html.png";
 import p2 from "../assets/css.png";
 import p3 from "../assets/react.png";
+import p4 from "../assets/ahfrog.png";
+import p5 from "../assets/ahtheary.png";
+import p6 from "../assets/ahmao.png";
 import "./pagehome.css";
 import { Link } from "react-router-dom";
 
 const PageHome = () => {
   return (
     <div className="wrapper">
-      <HomeProp img={p1} name="40$" detail="Samnag is a good boy">
+      <HomeProp img={p4} name="40$" detail="Samnag is a good boy">
         <Link to="html">HTML</Link>
       </HomeProp>
-      <HomeProp img={p2} name="40$" detail="Samnag is a good boy">
+      <HomeProp img={p5} name="40$" detail="Samnag is a good boy">
         <Link to="css">CSS</Link>
       </HomeProp>
-      <HomeProp img={p3} name="80$" detail="Samnag is a good boy">
+      <HomeProp img={p6} name="80$" detail="Samnag is a good boy">
         <Link to="react">ReactJS</Link>
       </HomeProp>
     </div>
